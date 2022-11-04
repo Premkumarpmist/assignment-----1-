65a9ef2449e385860a84231d96115d6463a39cb2
Premkumarpmist <116950176+Premkumarpmist@users.noreply.github.com>
2022-11-04 18:56:25 +0530

    Add files via upload

---
+++ top 30 schools web scraping.R
@@ -0,0 +1,11 @@
+library(rvest)
+library(dplyr)
+link='https://www.edudwar.com/top-and-best-schools-in-india/'
+web=read_html(link)
+schoolname=web%>%html_nodes(".grippy-host , a strong , .tp_btn_default")%>%html_text()
+View(schoolname)
+affiliation=web%>%html_node("ol+ p")%>%html_text()
+View(affiliation)
+schools=data.frame(schoolname,affiliation)
+View(schools)
+write.csv(collegs,'top 30 school.csv')
